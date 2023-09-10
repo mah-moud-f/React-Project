8843155b45293390d30f5954d04c804fc6455cda
hagarmorad2999 <hagarmorad92@gmail.com>
2023-09-10 16:02:41 +0300

    added profile page

---
+++ src/Component/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import CardOne from "../CardOne/CardOne";
+import CardTwo from "../CardTwo/CardTwo";
+
+
+export default function Layout(){
+return<>
+<div class="container-fluid">
+
+<CardOne/>
+<CardTwo/>
+
+</div>
+
+</>
+
+
+}
