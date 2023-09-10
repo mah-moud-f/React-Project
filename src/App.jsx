5e0fce9ef3b68e04fc9b93dd8674af815f9783fe
Mahmoud <mahmoudft8@gmail.com>
2023-09-10 08:21:08 +0300

    added react-router-dom package, and started to try handling some routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Feed from "./components/FeedComponent/Feed";
 import Header from "./components/HeaderComponent/Header";
 
 function App() {
+    let routers = createBrowserRouter([
+        {
+            path: "feed",
+            element: <Feed />,
+        },
+    ]);
+
     return (
         <>
             <Header />
-            <Feed />
+            <RouterProvider router={routers} />;
         </>
     );
 }
